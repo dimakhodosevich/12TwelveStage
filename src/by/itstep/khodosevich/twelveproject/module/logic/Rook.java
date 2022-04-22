c61be539a5db16c2312a6d98525dc9d8d88a6bc1
dimakhodosevich <102683640+dimakhodosevich@users.noreply.github.com>
2022-04-22 22:00:28 +0300

    Make logic and test for King figure. Make autoformatting.

--- src/by/itstep/khodosevich/twelveproject/module/logic/Rook.java
+++ src/by/itstep/khodosevich/twelveproject/module/logic/Rook.java
@@ -2,6 +2,9 @@ package by.itstep.khodosevich.twelveproject.module.logic;
 
 public class Rook {
     public static String isValidRookStep(int x1, int y1, int x2, int y2) {
-        return (y1 == y2 || x1 == x2) ? (x1 == x2 && y1 == y2 ? "NO" : "YES") : "NO";
+        if (x1 == x2 && y1 == y2) {
+            return "NO";
+        }
+        return (y1 == y2 || x1 == x2) ? "YES" : "NO";
     }
 }
--- test/by/itstep/khodosevich/twelveproject/module/logic/KingTest.java
+++ test/by/itstep/khodosevich/twelveproject/module/logic/KingTest.java
@@ -1,7 +1,35 @@
 package by.itstep.khodosevich.twelveproject.module.logic;
 
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static by.itstep.khodosevich.twelveproject.module.logic.King.*;
+
 public class KingTest {
-    public static String isValidKingStep(int x1, int y1, int x2, int y2) {
-        return "NO";
+
+    @Test
+    public void isValidKingStepPositiveYes() {
+        int[] startPoint = {5, 3};
+        int[][] stepPoint = {{5, 2}, {5, 4}, {4, 3}, {6, 3}, {4, 2}, {4, 4}, {6, 2}, {6, 4}};
+        String expected = "YES";
+
+        for (int i = 0; i < stepPoint.length; i++) {
+            String actual = isValidKingStep(startPoint[0], startPoint[1], stepPoint[i][0], stepPoint[i][1]);
+            assertEquals(expected, actual);
+        }
     }
+
+    @Test
+    public void isValidKingStepPositiveNo() {
+        int[] startPoint = {5, 3};
+        int[][] stepPoint = {{5, 5}, {3, 3}, {3, 5}, {7, 1}, {3, 1}, {1, 7}, {7, 5}, {7, 3}, {5, 3}};
+        String expected = "NO";
+
+        for (int i = 0; i < stepPoint.length; i++) {
+            String actual = isValidKingStep(startPoint[0], startPoint[1], stepPoint[i][0], stepPoint[i][1]);
+            assertEquals(expected, actual);
+        }
+    }
+
+
 }
