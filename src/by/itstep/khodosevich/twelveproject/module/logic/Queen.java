035e424fd2c98f9421d75f4198cb0b9d16c949d4
dimakhodosevich <102683640+dimakhodosevich@users.noreply.github.com>
2022-04-22 23:44:12 +0300

    Make logic and test for Queen figure. Make autoformatting.

---
+++ src/by/itstep/khodosevich/twelveproject/module/logic/Queen.java
@@ -0,0 +1,18 @@
+package by.itstep.khodosevich.twelveproject.module.logic;
+
+public class Queen {
+
+    public static String isValidQueenStep(int x1, int y1, int x2, int y2) {
+        if (x1 == x2 && y1 == y2) {
+            return "NO";
+        }
+
+        int x = x1 - x2;
+        x = x > 0 ? x : -x;
+        int y = y1 - y2;
+        y = y > 0 ? y : -y;
+
+        return (y1 == y2 || x1 == x2 || x == y) ? "YES" : "NO";
+    }
+
+}
---
+++ test/by/itstep/khodosevich/twelveproject/module/logic/QueenTest.java
@@ -0,0 +1,37 @@
+package by.itstep.khodosevich.twelveproject.module.logic;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static by.itstep.khodosevich.twelveproject.module.logic.Queen.*;
+
+public class QueenTest {
+
+
+    @Test
+    public void isValidQueenStepPositiveYes() {
+        int[] startPoint = {6, 4};
+        int[][] stepPoint = {{8, 2}, {7, 3}, {5, 5}, {4, 6}, {3, 7}, {2, 8}, {3, 1}, {4, 2}, {5, 3}, {7, 5}, {8, 6},
+                {5, 4}, {4, 4}, {3, 4}, {2, 4}, {1, 4}, {7, 4}, {8, 4}, {6, 3}, {6, 2}, {6, 1}, {6, 5}, {6, 6},
+                {6, 7}, {6, 8}};
+        String expected = "YES";
+
+        for (int i = 0; i < stepPoint.length; i++) {
+            String actual = isValidQueenStep(startPoint[0], startPoint[1], stepPoint[i][0], stepPoint[i][1]);
+            assertEquals(expected, actual);
+        }
+    }
+
+    @Test
+    public void isValidQueenStepPositiveNo() {
+        int[] startPoint = {6, 4};
+        int[][] stepPoint = {{6, 4}, {5, 2}, {2, 6}, {1, 1}, {8, 8}, {4, 5}, {5, 7}, {8, 3}};
+        String expected = "NO";
+
+        for (int i = 0; i < stepPoint.length; i++) {
+            String actual = isValidQueenStep(startPoint[0], startPoint[1], stepPoint[i][0], stepPoint[i][1]);
+            assertEquals(expected, actual);
+        }
+    }
+
+}
